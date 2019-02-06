ab1ebbcc7ee58d3623d709b9cebadc43746a479a
Corentin Gobbo <cgobbo@debian.univ-jfc.fr>
2019-02-06 11:22:58 +0100

    Projet avec bebou

---
+++ src/tuto_git/Application.java
@@ -0,0 +1,71 @@
+package tuto_git;
+
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Application extends Frame implements WindowListener {
+
+	public static void main(String[] args) {
+		new Application();
+	}
+	
+	public Application() {
+		super();
+		
+		Modèle modl = new Modèle();
+		Contrôleur ctrl = new Contrôleur(modl); 
+		Vue vue = new Vue();
+		modl.addObserver(vue);
+		vue.addActionListener(ctrl);
+		this.setTitle("tutoriel git");
+		this.add(vue);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
